33b666438658480a8c7f26179c044e59506b929b
Moises Baltazar <me@moisesb.com>
2019-08-15 04:53:02 -0500

    Add ability to do simple property conversions

---
+++ src/properties.js
@@ -0,0 +1,26 @@
+import { simple as simple_inv } from 'invertible'
+
+/**
+ *
+ * @param {string} x
+ * @param {string} y
+ * @returns {{}}
+ */
+export function convert_prop(x, y) {
+    return simple_inv({
+        context: { read_prop: [x, y], write_prop: [y, x] },
+        fn: __internal_property_conversion_fn__,
+    })
+}
+
+function __internal_property_conversion_fn__(args) {
+    const { input, output } = args
+    const value = input[this.read_prop]
+
+    if (value === undefined)
+        // edge-case: delete property created by a previous rule
+        delete output[this.write_prop]
+    else output[this.write_prop] = value
+
+    return args
+}
---
+++ test/properties.spec.js
@@ -0,0 +1,37 @@
+import 'chai/register-should'
+import { convert_prop } from '../src/properties'
+import seq from '../src/sequence'
+
+describe('properties.js', () => {
+    it('can do simple object property mapping', () => {
+        const propCon = seq(
+            convert_prop('old', 'new'),
+            convert_prop('black', 'white'),
+            convert_prop('ying', 'yang'),
+        )
+
+        // forwards
+        propCon({
+            old: 9,
+            black: true,
+            ying: 'Hi ying!',
+        }).should.deep.equal({
+            new: 9,
+            white: true,
+            yang: 'Hi ying!',
+        })
+
+        // reverse
+        propCon
+            .inv({
+                new: 'baby',
+                white: 'elephant',
+                yang: 'Hello yang!',
+            })
+            .should.deep.equal({
+                old: 'baby',
+                black: 'elephant',
+                ying: 'Hello yang!',
+            })
+    })
+})
